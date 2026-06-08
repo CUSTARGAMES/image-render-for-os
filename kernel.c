3dda4ecd93c2f45deed278ea1ef24b6a076ab357
saadmann <mtaslimaroja@gmail.com>
2026-06-08 17:35:47 +0600

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,10 +1,9 @@
-/* Simple BMP Viewer OS - All in one file */
+/* Simple BMP Viewer OS */
 
 #define VGA_WIDTH  640
 #define VGA_HEIGHT 480
-#define VGA_MEMORY 0xA0000
 
-/* Multiboot module structure */
+/* Multiboot structures */
 typedef struct {
     unsigned int mod_start;
     unsigned int mod_end;
@@ -12,7 +11,6 @@ typedef struct {
     unsigned int pad;
 } module_t;
 
-/* Multiboot info structure */
 typedef struct {
     unsigned int flags;
     unsigned int mem_lower;
@@ -23,77 +21,111 @@ typedef struct {
     unsigned int mods_addr;
 } multiboot_info_t;
 
-/* BMP header structures */
+/* BMP header */
 #pragma pack(push, 1)
 typedef struct {
-    unsigned short signature;   /* 'BM' */
-    unsigned int   file_size;
+    unsigned short signature;
+    unsigned int file_size;
     unsigned short reserved1;
     unsigned short reserved2;
-    unsigned int   data_offset;
-} bmp_header_t;
-
-typedef struct {
-    unsigned int   header_size;
-    int            width;
-    int            height;
+    unsigned int data_offset;
+    unsigned int header_size;
+    int width;
+    int height;
     unsigned short planes;
     unsigned short bpp;
-    unsigned int   compression;
-    unsigned int   image_size;
-    int            x_pixels_per_meter;
-    int            y_pixels_per_meter;
-    unsigned int   colors_used;
-    unsigned int   colors_important;
-} bmp_info_header_t;
-
-typedef struct {
-    unsigned char b;
-    unsigned char g;
-    unsigned char r;
-    unsigned char reserved;
-} bmp_palette_entry_t;
+    unsigned int compression;
+    unsigned int image_size;
+    int x_pixels_per_meter;
+    int y_pixels_per_meter;
+    unsigned int colors_used;
+    unsigned int colors_important;
+} bmp_header_t;
 #pragma pack(pop)
 
-/* Simple string functions */
-int strcmp(const char *s1, const char *s2) {
-    while (*s1 && *s2 && *s1 == *s2) { s1++; s2++; }
-    return *s1 - *s2;
+/* VGA registers */
+#define VGA_AC_INDEX   0x3C0
+#define VGA_AC_WRITE   0x3C0
+#define VGA_AC_READ    0x3C1
+#define VGA_DAC_WRITE  0x3C8
+#define VGA_DAC_DATA   0x3C9
+#define VGA_INSTAT     0x3DA
+
+/* Set VGA 640x480 16-color mode */
+void set_vga_mode() {
+    __asm__ volatile(
+        "mov $0x0012, %%ax\n"
+        "int $0x10\n"
+        : : : "ax"
+    );
 }
 
-unsigned int strlen(const char *s) {
-    unsigned int len = 0;
-    while (*s++) len++;
-    return len;
+/* Set one palette color */
+void set_palette_color(int index, unsigned char r, unsigned char g, unsigned char b) {
+    __asm__ volatile("outb %0, %1" : : "a"((unsigned char)index), "Nd"(VGA_DAC_WRITE));
+    __asm__ volatile("outb %0, %1" : : "a"(r), "Nd"(VGA_DAC_DATA));
+    __asm__ volatile("outb %0, %1" : : "a"(g), "Nd"(VGA_DAC_DATA));
+    __asm__ volatile("outb %0, %1" : : "a"(b), "Nd"(VGA_DAC_DATA));
 }
 
-/* Write to VGA register */
-static void vga_write_reg(unsigned short port, unsigned char index, unsigned char value) {
-    /* Some VGA registers need different handling */
-    if (port == 0x3C0) {
-        __asm__ volatile("inb $0x3DA, %%al\n\t" : : : "al");
-        __asm__ volatile("outb %0, $0x3C0" : : "a"(index));
-        __asm__ volatile("outb %0, $0x3C0" : : "a"(value));
-    } else {
-        __asm__ volatile("outb %0, %w1" : : "a"(index), "Nd"(port));
-        __asm__ volatile("outb %0, %w1" : : "a"(value), "Nd"((unsigned short)(port + 1)));
+/* Display BMP at given address */
+void display_bmp(unsigned char *bmp_data) {
+    bmp_header_t *header = (bmp_header_t *)bmp_data;
+    
+    /* Check signature */
+    if (header->signature != 0x4D42) return;
+    if (header->bpp != 8) return;
+    
+    int width = header->width;
+    int height = header->height;
+    
+    /* Load palette */
+    unsigned char *palette = bmp_data + 54; /* 54 = sizeof(bitmap header) */
+    for (int i = 0; i < 256; i++) {
+        set_palette_color(i, 
+            palette[i * 4 + 2] >> 2,  /* Red */
+            palette[i * 4 + 1] >> 2,  /* Green */
+            palette[i * 4 + 0] >> 2   /* Blue */
+        );
+    }
+    
+    /* Get pixel data */
+    unsigned char *pixels = bmp_data + header->data_offset;
+    unsigned char *vga = (unsigned char *)0xA0000;
+    
+    /* BMP is bottom-up */
+    for (int y = height - 1; y >= 0; y--) {
+        for (int x = 0; x < width; x++) {
+            /* Simple mapping - each byte = 2 pixels in 16-color mode */
+            int pixel = pixels[y * width + x];
+            int offset = y * (VGA_WIDTH / 8) + x / 8;
+            int bit = 7 - (x % 8);
+            
+            unsigned char mask = 0x80 >> bit;
+            if (pixel & 1) {
+                vga[offset] |= mask;
+            } else {
+                vga[offset] &= ~mask;
+            }
+        }
     }
 }
 
-/* Set 640x480 16-color VGA mode (mode 0x12) */
-void set_vga_mode(void) {
-    /* Disable interrupts during mode switch */
-    __asm__ volatile("cli");
+/* Main kernel entry */
+void kernel_main(unsigned int magic, unsigned int addr) {
+    set_vga_mode();
     
-    /* Set mode 0x12 (640x480 16 colors) */
-    __asm__ volatile(
-        "mov $0x0012, %%ax\n\t"
-        "int $0x10\n\t"
-        : : : "ax"
-    );
+    multiboot_info_t *mbi = (multiboot_info_t *)addr;
     
-    __asm__ volatile("sti");
+    /* Check if modules were loaded */
+    if (mbi->mods_count > 0) {
+        module_t *mod = (module_t *)mbi->mods_addr;
+        unsigned char *bmp_data = (unsigned char *)mod->mod_start;
+        display_bmp(bmp_data);
+    }
+    
+    /* Hang forever */
+    while(1) {
+        __asm__ volatile("hlt");
+    }
 }
-
-/* Set VGA palette */
-void set_palette(unsigned char *palette, int num
